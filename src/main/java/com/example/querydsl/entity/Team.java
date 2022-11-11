9d03e41497242f6d1694177f093ce057741661ae
taekgyu <taekgyu0602@gmail.com>
2022-11-11 21:20:03 +0900

    Member, Team 연관관계 맺기 및 테스트

---
+++ src/main/java/com/example/querydsl/entity/Team.java
@@ -0,0 +1,29 @@
+package com.example.querydsl.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자를 PROTECTED 레벨로 생성
+@ToString(of = {"of", "name"}) // 무한 참조 될 수 있기 때문에 연관관계 있는 필드는 제외하고 등록하자
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+
+}
